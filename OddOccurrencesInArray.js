49efa7ea56df5c2d690a76d20df7d813a5133d05
Tiago Donatti <tdonatti777@gmail.com>
2017-12-16 11:18:49 -0300

    OddOccurrencesInArray

---
+++ OddOccurrencesInArray.js
@@ -0,0 +1,29 @@
+
+// Link to task result  => https://codility.com/demo/results/trainingNBNR4S-9SJ/
+// Link to Repl.it      => https://repl.it/@tdonatti/OddOccurrencesInArray
+
+function solution(A) {
+    
+        const collection = A.sort();
+        let result = [];
+        
+        for(const number of collection) {
+          if(result[result.length - 1] != number) {
+            result.push(number)
+          } else {
+            result.pop(result.last)
+          }
+        }
+      
+        return Number(result);
+    }
+    
+    const ArrColl = [
+      [9, 7, 9],
+      [9, 3, 9, 3, 9, 7, 9],
+      [1, 9, 3, 9, 3, 9, 7, 9, 7],
+      [1, 9, 3, 9, 9, 7, 9, 7, 1, 15, 3, 4, 4],
+      [1]
+      ]
+    
+    ArrColl.forEach((Arr) => console.log(solution(Arr)))
