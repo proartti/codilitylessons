f4d49ed3690242315d571eed7310664f72d8936b
Tiago Donatti <tdonatti777@gmail.com>
2017-12-17 13:06:17 -0300

    Frog Jumps

---
+++ FrogJmp.js
@@ -0,0 +1,16 @@
+
+// Link to task result  => https://codility.com/demo/results/training34X8FF-V3N/
+// Link to Repl.it      => https://repl.it/@tdonatti/FrogJmp
+
+
+function solution(X, Y, D) {
+    return Math.ceil((Y - X) / D); 
+  }
+  
+  frogJups = [
+    [10, 85, 30],
+    [1, 2, 1],
+    [123, 42433, 4],
+  ];
+  
+  frogJups.forEach(N => console.log(solution(N[0], N[1], N[2])))
