1d8d506508ba0fd6acbefbef1745f387951f8fa7
Tiago Donatti <tdonatti777@gmail.com>
2017-12-17 13:10:54 -0300

    Fixed wrong indentation

--- CyclicRotation.js
+++ CyclicRotation.js
@@ -3,27 +3,26 @@
 // Link to Repl.it      => https://repl.it/@tdonatti/CyclicRotation
 
 function solution(A, K) {
+
+    if(A.length == 0) {
+        return A;
+    }
+
+    const collection = A;
+    const cycles = K;
     
-      if(A.length == 0) {
-          return A;
-      }
-    
-      const collection = A;
-      const cycles = K;
-      
-      for(let i = 0; i < cycles; i++) {
+    for(let i = 0; i < cycles; i++) {
         let shiftNumber = collection.pop(collection.length - 1);
         collection.unshift(shiftNumber);
-      }
-      
-      return collection;
-  }
-  
-  testCases = [
+    }
+    
+    return collection;
+}
+
+testCases = [
     [[1, 2, 3, 4, 5], 4],
     [[1, 2, 3, 4, 5, 6, 7, 8, 9, 0], 1],
     [[], 0]
-    ];
-  
-  testCases.forEach(N => console.log(solution(N[0], N[1])))
-  
+];
+
+testCases.forEach(N => console.log(solution(N[0], N[1])))
--- OddOccurrencesInArray.js
+++ OddOccurrencesInArray.js
@@ -3,27 +3,27 @@
 // Link to Repl.it      => https://repl.it/@tdonatti/OddOccurrencesInArray
 
 function solution(A) {
-    
-        const collection = A.sort();
-        let result = [];
-        
-        for(const number of collection) {
-          if(result[result.length - 1] != number) {
-            result.push(number)
-          } else {
-            result.pop(result.last)
-          }
-        }
-      
-        return Number(result);
+
+  const collection = A.sort();
+  let result = [];
+
+  for(const number of collection) {
+    if(result[result.length - 1] != number) {
+      result.push(number)
+    } else {
+      result.pop(result.last)
     }
-    
-    const ArrColl = [
-      [9, 7, 9],
-      [9, 3, 9, 3, 9, 7, 9],
-      [1, 9, 3, 9, 3, 9, 7, 9, 7],
-      [1, 9, 3, 9, 9, 7, 9, 7, 1, 15, 3, 4, 4],
-      [1]
-      ]
-    
-    ArrColl.forEach((Arr) => console.log(solution(Arr)))
+  }
+
+  return Number(result);
+}
+
+const ArrColl = [
+  [9, 7, 9],
+  [9, 3, 9, 3, 9, 7, 9],
+  [1, 9, 3, 9, 3, 9, 7, 9, 7],
+  [1, 9, 3, 9, 9, 7, 9, 7, 1, 15, 3, 4, 4],
+  [1]
+];
+
+ArrColl.forEach((Arr) => console.log(solution(Arr)))
