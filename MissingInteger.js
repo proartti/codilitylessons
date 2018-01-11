7fa1ff296579929ad6e52086d18dd64727334a0d
Tiago Donatti <tdonatti777@gmail.com>
2018-01-11 10:33:27 -0300

    MissingInteger

---
+++ MissingInteger.js
@@ -0,0 +1,31 @@
+
+// Link to task result  => https://app.codility.com/demo/results/training5A48FT-4A9/
+// Link to Repl.it      => https://repl.it/@tdonatti/MissingInteger
+
+function solution(X, A) {
+    var map = {};
+    var min = 1;
+    
+    for(var i = 0; i < A.length; i++) {
+        if(A[i] > 0) {
+            map[A[i]] = true;
+        }
+    }
+    
+    console.log(map)
+
+    if(!map[min]) return 1;
+
+    while(map[min]) min++;
+
+    return min;
+}
+
+testCases = [
+    [-1, 0, 1, 3, 6, 4, 1, 2],
+    [1, 2, 3],
+    [-1, -3]
+];
+
+testCases.forEach(N => console.log(solution(N)));
+
